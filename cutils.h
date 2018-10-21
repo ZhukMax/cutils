f591d5a2117f244882f4a7a3c23b41b6f3749eee
Max Zhuk <zhukmax@ya.ru>
2018-10-21 12:20:36 +0300

    Update cutils.h

--- cutils.h
+++ cutils.h
@@ -7,12 +7,4 @@
 #define countLD(x) (sizeof(x)/sizeof(long double))
 #define countS(x) (sizeof(x))
 
-#define printBits(x) \
-            int i = sizeof(x)*8-1; \
-            while(i >= 0) { \
-                printf("%d", x&(1<<i)?1:0); \
-                if ((i&7) == 0) printf(" "); \
-                i--; \
-            }
-
 #endif
