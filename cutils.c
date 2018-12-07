e3da974783d1ca8c15dbb3bbc8ba61bf66a08ef2
Max Zhuk <zhukmax@ya.ru>
2018-12-07 13:54:26 +0300

    Update cutils.c

--- cutils.c
+++ cutils.c
@@ -1,3 +1,12 @@
 #include "cutils.h"
 
-void printBits(int x) {}
+void printBits(char *x, int qty) {
+    for (int j = 0; j < qty; ++j) {
+        int i = sizeof(x[j])*8-1;
+        while(i >= 0) {
+            printf("%d", x[j]&(1<<i)?1:0);
+            if ((i&7) == 0) printf(" ");
+            i--;
+        }
+    }
+}
